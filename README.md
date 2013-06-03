63185f0f05da06765eb5124484efc759144e4447
Matteo Figus <matteo@swapit.co.uk>
2013-06-03 15:28:21 +0100

    Added a test

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ nice-json2csv
 =============
 
 A simple tool for converting json to csv - it doesn't require headers
+
+# Installation
+
+	npm install nice-json2csv
+
+# Usage
+
+	var express = require('express');
+	var json2csv = require('nice-json2csv');
+
+	var app = express();
+
+	app.get('/', function(req, res){
+		var myData = [{ "first_name": "John", "last_name": "Doe"}, { "first_name": "Jane", "last_name": "Doe"}, { "first_name": "Mick"}];
+		
+		res.csv(myData, "filename.csv");
+	});
+
+	app.listen(3000);
--- test/res.csv.js
+++ test/res.csv.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var superagent = require('superagent');
 var should = require('should');
 var _ = require('underscore');
 
@@ -11,5 +12,23 @@ describe('res.csv()', function(){
   it('should correctly extend res', function(done) {
     res.csv.should.be.a('function');
     done();
+  });
+
+  it('should correctly return my csv when invoked through express', function(done) {
+
+    var app = express();
+    var myData = [{ "a": "b", "c": "d"}, { "a": "e", "c": "f"}];
+      
+    app.get('/test', function(req, res){
+      res.csv(myData, "filename.csv");
+    });
+
+    app.listen(3009, function(){
+      superagent.get("http://127.0.0.1:3009/test").end(function(res){
+        res.headers['content-disposition'].should.be.eql('attachment; filename=filename.csv');
+        res.text.should.eql("\"a\",\"c\"\n\"b\",\"d\"\n\"e\",\"f\"");
+        done();
+      });
+    });
   });
 });
